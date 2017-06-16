4e37b06171679560ff04e4dd706845ad47a2f832
Robert Desrosiers <robert.desrosiers@teamgiga.com>
2017-06-15 20:57:34 -0400

    v0.0.0.0 - Project Setup
    Configuring repo folder setup.

---
+++ Goldman/Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject target;
+    public float followAhead;
+    public float cameraSmooth;
+
+    private Vector3 targetPos;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        targetPos = new Vector3(target.transform.position.x, transform.position.y, transform.position.z);
+
+        // Shift camera target slightly depending on where the player is looking.
+        if (target.transform.localScale.x > 0f)
+        {
+            targetPos = new Vector3(targetPos.x + followAhead, targetPos.y, targetPos.z);
+        }
+        else
+        {
+            targetPos = new Vector3(targetPos.x - followAhead, targetPos.y, targetPos.z);
+        }
+
+        // Smooth the camera's movement.
+        transform.position = Vector3.Lerp(transform.position, targetPos, cameraSmooth * Time.deltaTime);
+
+    }
+}
---
+++ Goldman/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
+    public float moveSpeed;
+    public float jumpSpeed;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    public bool isGrounded;
+
+    void Start () {
+
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
+	
+	void Update () {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        Jump();
+        Movement();
+
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb2d.velocity = new Vector3(rb2d.velocity.x, jumpSpeed, 0f);
+        }
+    }
+
+    void Movement()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0f)
+        {
+            rb2d.velocity = new Vector3(moveSpeed, rb2d.velocity.y, 0f);
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0f)
+        {
+            rb2d.velocity = new Vector3(-moveSpeed, rb2d.velocity.y, 0f);
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else
+        {
+            rb2d.velocity = new Vector3(0, rb2d.velocity.y, 0f);
+        }
+    }
+
+    void Animation()
+    {
+        //anim.SetFloat("MoveSpeed", Mathf.Abs(rb2d.velocity.x));
+        //anim.SetBool("Grounded", isGrounded);
+    }
+}
